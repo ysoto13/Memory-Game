a42045600d6420069db50f3816b1f7052b67935f
Yadira Soto <lunar13heart@hotmail.com>
2019-07-30 22:56:47 -0400

    Updates

---
+++ precache-manifest.ec8a079f94a144d69c8eddf6cbec8a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55faa5c02421129ef67d1bb6e9e3ae37",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "3938f3844e83e2914ea1",
+    "url": "/Memory-Game/static/css/main.9aa38ee9.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Memory-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3938f3844e83e2914ea1",
+    "url": "/Memory-Game/static/js/main.d22fc90b.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "c614347ab1c7c0ae96b267c57f666aef",
+    "url": "/Memory-Game/static/media/full-bloom.c614347a.png"
+  }
+]);
